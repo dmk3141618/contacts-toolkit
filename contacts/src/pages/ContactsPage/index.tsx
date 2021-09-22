5b81cf14cb94bc81eae122458218b2844ca13848
dmk3141618 <dmk3141618@gmail.com>
2021-09-22 18:36:52 +0900

    feat(ContactsPage): add contactForm using react-hook-form with yup
    Almost done ContactsPage design using scss module
    yarn add react-hook-form yup @hookform/resolvers

--- contacts/src/pages/ContactsPage/index.tsx
+++ contacts/src/pages/ContactsPage/index.tsx
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, {useCallback, useRef, useState} from 'react';
 import {RouteComponentProps} from 'react-router-dom';
 import styles from '~/pages/ContactsPage/ContactsPage.module.scss';
 import Button from '~/common/component/Button';
 import Icon from '~/common/component/Icon';
+import Dialog from '~/common/component/Dialog';
+import DialogFull from '~/common/component/DialogFull';
+import {useForm} from 'react-hook-form';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
+
+interface ContactFormValuesType {
+  name: string;
+  email: string;
+  phone: string;
+}
 
 type Props = RouteComponentProps;
 function ContactsPage({}: Props) {
+  const [showDeleteConfirmDialog, setShowDeleteConfirmDialog] = useState<boolean>(false);
+  const onClickDeleteConfirm = useCallback(() => {
+    setShowDeleteConfirmDialog(true);
+  }, []);
+  const onConfirmDeleteConfirm = useCallback(() => {
+    setShowDeleteConfirmDialog(false);
+  }, []);
+  const onCancelDeleteConfirm = useCallback(() => {
+    setShowDeleteConfirmDialog(false);
+  }, []);
+
+  const contactForm = useRef<HTMLFormElement>(null);
+  const [showContactFormDialog, setShowContactFormDialog] = useState<boolean>(false);
+  const onClickContactForm = useCallback(() => {
+    setShowContactFormDialog(true);
+  }, []);
+  const onConfirmContactForm = useCallback(() => {
+    contactForm.current?.requestSubmit();
+  }, []);
+  const onCancelContactForm = useCallback(() => {
+    setShowContactFormDialog(false);
+  }, []);
+  const contactFormValidation = yup.object().shape({
+    name: yup.string().required('Required'),
+    email: yup.string().email('Invalid email address').required('Required'),
+    phone: yup.lazy(value =>
+      value === ''
+        ? yup.string()
+        : yup
+            .number()
+            .typeError('Numbers only')
+            .test('len', 'At least 10 digit', val => !val || val.toString().length >= 10)
+            .test('len', 'Max 20 digit', val => !val || val.toString().length <= 20),
+    ),
+  });
+  const {
+    register,
+    formState: {errors},
+    handleSubmit,
+  } = useForm<ContactFormValuesType>({
+    resolver: yupResolver(contactFormValidation),
+    mode: 'onBlur',
+  });
+  const onSubmitContactForm = handleSubmit((data: ContactFormValuesType) => {
+    console.log(data);
+  });
+
   return (
     <div className={styles.pageWrap}>
       <div className={styles.appBlock}>
@@ -18,10 +76,10 @@ function ContactsPage({}: Props) {
               <label>Total</label>
               <span>1124</span>
             </div>
-            <div className={styles.controlSortBy}>
-              <label>Sort By</label>
-              <span>created</span>
-            </div>
+            {/*<div className={styles.controlSortBy}>*/}
+            {/*  <label>Sort By</label>*/}
+            {/*  <span>created</span>*/}
+            {/*</div>*/}
             <div className={styles.controlFavorites}>
               <div className={styles.iconWrap}>
                 <Icon className={styles.icon} icon={'heartBlank'} />
@@ -30,11 +88,10 @@ function ContactsPage({}: Props) {
             </div>
           </div>
           <div className={styles.controlBtnsWrap}>
-            <Button size={'medium'}>Add Contact</Button>
+            <Button size={'medium'} onClick={onClickContactForm}>
+              Add Contact
+            </Button>
           </div>
-        </div>
-        <div className={styles.addFormWrap}>
-          <div className={styles.addForm}>addForm</div>
         </div>
         <div className={styles.listWrap}>
           <ul className={styles.list}>
@@ -46,19 +103,19 @@ function ContactsPage({}: Props) {
                     <span>Hong Gil Dong</span>
                   </div>
                   <div className={styles.listItemContentField}>
-                    <label>phone</label>
-                    <span>01012340987</span>
-                  </div>
-                  <div className={styles.listItemContentField}>
                     <label>email</label>
                     <span>test@gmail.com</span>
+                  </div>
+                  <div className={styles.listItemContentField}>
+                    <label>phone</label>
+                    <span>01012340987</span>
                   </div>
                 </div>
                 <div className={styles.listItemBtnsWrap}>
                   <Button size={'small'} outline={true}>
                     Edit
                   </Button>
-                  <Button size={'small'} outline={true}>
+                  <Button size={'small'} outline={true} onClick={onClickDeleteConfirm}>
                     Delete
                   </Button>
                   <div>
@@ -75,19 +132,19 @@ function ContactsPage({}: Props) {
                     <span>Hong Gil Dong</span>
                   </div>
                   <div className={styles.listItemContentField}>
-                    <label>phone</label>
-                    <span>01012340987</span>
-                  </div>
-                  <div className={styles.listItemContentField}>
                     <label>email</label>
                     <span>test@gmail.com</span>
+                  </div>
+                  <div className={styles.listItemContentField}>
+                    <label>phone</label>
+                    <span>01012340987</span>
                   </div>
                 </div>
                 <div className={styles.listItemBtnsWrap}>
                   <Button size={'small'} outline={true}>
                     Edit
                   </Button>
-                  <Button size={'small'} outline={true}>
+                  <Button size={'small'} outline={true} onClick={onClickDeleteConfirm}>
                     Delete
                   </Button>
                   <div>
@@ -97,8 +154,52 @@ function ContactsPage({}: Props) {
               </div>
             </li>
           </ul>
+          <div className={styles.emptyWrap}>
+            <div className={styles.emptyTitle}>No Contact</div>
+            <div className={styles.emptyText}>Will you add a new contact?</div>
+          </div>
         </div>
       </div>
+      <Dialog
+        title="Delete"
+        confirmText="Delete"
+        cancelText="Cancel"
+        onConfirm={onConfirmDeleteConfirm}
+        onCancel={onCancelDeleteConfirm}
+        visible={showDeleteConfirmDialog}
+      >
+        Are you sure to delete?
+      </Dialog>
+      <DialogFull
+        title="Add new contact"
+        confirmText="Confirm"
+        cancelText="Cancel"
+        onConfirm={onConfirmContactForm}
+        onCancel={onCancelContactForm}
+        visible={showContactFormDialog}
+      >
+        <div className={styles.contactFormWrap}>
+          <div className={styles.contactForm}>
+            <form ref={contactForm} onSubmit={onSubmitContactForm}>
+              <div className={styles.field}>
+                <label htmlFor="name">Name</label>
+                <input id="name" {...register('name')} autoComplete="off" />
+                <div className={styles.error}>{errors.name?.message}</div>
+              </div>
+              <div className={styles.field}>
+                <label htmlFor="email">Email</label>
+                <input id="email" {...register('email')} autoComplete="off" />
+                <div className={styles.error}>{errors.email?.message}</div>
+              </div>
+              <div className={styles.field}>
+                <label htmlFor="phone">Phone</label>
+                <input id="phone" {...register('phone')} autoComplete="off" />
+                <div className={styles.error}>{errors.phone?.message}</div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </DialogFull>
     </div>
   );
 }
